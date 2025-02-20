d89ed440e8c5320c6350cab8071781deaf227791
MLijffijt <mg_lijffijt@ziggo.nl>
2025-02-20 12:41:09 +0100

    1

--- assets/js/menu.js
+++ assets/js/menu.js
@@ -1,34 +1,23 @@
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Get the modal
+document.addEventListener('DOMContentLoaded', () => {
     var modal = document.getElementById("loginModal");
-
-    // Get the button that opens the modal
     var loginBtn = document.querySelector(".log-in");
+    var closeModal = document.querySelector(".close");
 
-    // Get the <span> element that closes the modal
-    var closeModal = document.getElementsByClassName("close")[0];
-
-    // Ensure the modal is hidden initially
-    modal.style.display = "none";
-
-    // When the user clicks the login button, open the modal 
     loginBtn.onclick = function() {
         modal.style.display = "flex";
     }
 
-    // When the user clicks on <span> (x), close the modal
     closeModal.onclick = function() {
         modal.style.display = "none";
     }
 
-    // When the user clicks anywhere outside of the modal, close it
     window.onclick = function(event) {
         if (event.target == modal) {
             modal.style.display = "none";
         }
     }
 
-    // Sidebar functionality
+    // Sidebar
     var sidebar = document.getElementById("sidebar");
     var menuBtn = document.querySelector(".menu");
 
